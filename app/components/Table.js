dc0db9dc7e90e9bd63b0d49060e5a5bf101e9b8a
Shaii Ong <monsteronfire@gmail.com>
2017-07-16 23:32:27 +0800

    Start using data in Table

--- app/components/Table.js
+++ app/components/Table.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data
+    };
+  }
+
   render() {
     return (
-      <div className='table'></div>
+      <div className='table'>
+        {this.state.data.map((item) => {
+          item
+        })}
+      </div>
     )
   }
 }
